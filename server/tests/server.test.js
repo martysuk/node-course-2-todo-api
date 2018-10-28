bb5564fac908a4b2ebe6c146f32308468e40f33a
marta <martysuk99@gmail.com>
2018-10-28 21:22:19 +0200

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [
     },
     {
         _id: new ObjectID(),
-        text: 'Second test todo'
+        text: 'Second test todo',
+        completed: true,
+        completedAt: 123
     }
 ]
 
@@ -141,5 +143,45 @@ describe('DELETE /todos/:id', () => {
             .expect(404)
             .end(done)
 
+    })
+})
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        let id = todos[0]._id.toHexString()
+        let text = 'This should be the new text'
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.completed).toBe(true)
+                expect(res.body.todo.text).toBe(text)
+                expect(typeof res.body.todo.completedAt).toBe('number')
+            })
+            .end(done)
+    })
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        let id = todos[1]._id.toHexString()
+        let text = 'This should be the new text!!'
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(text)
+                expect(res.body.todo.completed).toBe(false)
+                expect(res.body.todo.completedAt).toBeFalsy()
+            })
+            .end(done)
     })
 })
